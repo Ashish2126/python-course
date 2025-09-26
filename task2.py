5304ab468c56d81a631956b884bc79ebeb744b1e
Ashish2126 <ashishsharma.vitk@gmail.com>
2025-09-26 11:04:31 +0530

    assignment 3
    task 1= calculate the factorial of number
    task 2= using the math module for calculations

---
+++ task2.py
@@ -0,0 +1,8 @@
+import math
+n= float(input("enter a number:"))
+sqrt= math.sqrt(n)
+logirithm = math.log(n)
+sine_value= math. sin(n)
+print(f"square root:{sqrt}")
+print(f"logirithm:{logirithm}")
+print(f"sine:{sine_value}")
